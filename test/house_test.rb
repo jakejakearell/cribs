68cd6b345be98a902e55b90908dbf0521a722394
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-09 13:48:43 -0700

    add tests for iteration 3

--- test/house_test.rb
+++ test/house_test.rb
@@ -8,6 +8,9 @@ class HouseTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, '13')
     @room_2 = Room.new(:bedroom, 11, '15')
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
+
   end
 
   def test_it_exists
@@ -29,4 +32,25 @@ class HouseTest < Minitest::Test
     assert_equal [@room_1, @room_2], @house.rooms
   end
 
+  def test_can_tell_if_house_above_market_average
+    assert_equal false, @house.above_market_average?
+
+  end
+
+  def test_sorting_rooms
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+
+    assert_equal [@room_2, @room_2], @house.rooms_from_category(:bedroom)
+
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
+
+
+    assert_equal 1900, @house.area
+
+    # assert_equal {"price" => 400000, "address" => "123 sugar lane"}, @house.details
+
+  end
 end
