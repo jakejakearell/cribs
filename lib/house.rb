b886413a4c871f58e82f9ec2e27b0f05b6fd10b4
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-09 14:02:49 -0700

    add code for itteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -13,8 +13,49 @@ class House
 
   end
 
+  def above_market_average?
+    price > 500000
+  end
+
   def add_room(room)
     @rooms << room
   end
+
+  def rooms_from_category(type)
+    list = []
+
+    @rooms.each do |room|
+      if room.category == type
+        list << room
+      end
+    end
+
+    list
+
+  end
+
+  def area
+    house_area = 0
+
+    @rooms.each do |room|
+      house_area += room.area
+    end
+
+    house_area
+
+  end
+
+  def details
+    details = Hash.new
+
+    details["price"] = price
+    details['address'] = @address
+
+    details
+  end
+
+
+
+
 
 end
